e7861743a8425353bc22cfc7449c72cc2b2b0a43
tuleateodoraflorina <tuleateodora23@stud.ase.ro>
2026-01-09 20:19:04 +0200

    update to the file

--- app/src/main/java/virtualcompanionforfinancialactivities/licenta/dao/TransactionDao.java
+++ app/src/main/java/virtualcompanionforfinancialactivities/licenta/dao/TransactionDao.java
@@ -5,7 +5,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 import java.util.List;
-import virtualcompanionforfinancialactivities.licenta.Transaction;
+import virtualcompanionforfinancialactivities.Transaction;
 
 @Dao
 public interface TransactionDao<Transaction> {
